2bc52a2685369327bedc9606948c5bf1f8f043c2
Justin Anderson <justand@gmail.com>
2020-02-24 10:41:12 -0800

    Add grid section to DOM

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// Card data
+const cardsArray = [
+  {
+    name: 'shell',
+    img: 'img/blueshell.png'
+  },
+  {
+    name: 'star',
+    img: 'img/star.png'
+  },
+  {
+    name: 'bobomb',
+    img: 'img/bobomb.png'
+  },
+  {
+    name: 'mario',
+    img: 'img/mario.png'
+  },
+  {
+    name: 'luigi',
+    img: 'img/luigi.png'
+  },
+  {
+    name: 'peach',
+    img: 'img/peach.png'
+  },
+  {
+    name: '1up',
+    img: 'img/1up.png'
+  },
+  {
+    name: 'mushroom',
+    img: 'img/mushroom.png'
+  },
+  {
+    name: 'thwomp',
+    img: 'img/thwomp.png'
+  },
+  {
+    name: 'bulletbill',
+    img: 'img/bulletbill.png'
+  },
+  {
+    name: 'coin',
+    img: 'img/coin.png'
+  },
+  {
+    name: 'goomba',
+    img: 'img/goomba.png'
+  }
+];
+
+// Grab the div with an id of game
+const game = document.getElementById('game');
+
+// Create a section with a class of grid
+const grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+
+// Append the grid section to the game div
+game.appendChild(grid);
